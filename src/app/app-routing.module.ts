0d55f05017dd5860b9fde09b20cc2ac4c9c2f427
MarcosCarvajal <johndoe@example.com>
2024-05-21 17:27:39 -0500

    cambio de color fondo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'inicio',
+    component: AppComponent
+  },
+  {
+    path:'**',
+    redirectTo:'inicio',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
